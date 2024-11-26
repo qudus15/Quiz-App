ec21b585e1af240d84b0f4a4d65279c62a51e0c9
qudus15 <meet.qudus@gmail.com>
2024-11-26 17:45:46 -0500

    Initial Commit

---
+++ quiz/quizApp.swift
@@ -0,0 +1,17 @@
+//
+//  quizApp.swift
+//  quiz
+//
+//  Created by Red Whale inc. on 26/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct quizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ quizTests/quizTests.swift
@@ -0,0 +1,17 @@
+//
+//  quizTests.swift
+//  quizTests
+//
+//  Created by Red Whale inc. on 26/11/2024.
+//
+
+import Testing
+@testable import quiz
+
+struct quizTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
