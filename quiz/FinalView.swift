7473aa682efa7d7fc5e81e6fff05759d6c2a645f
qudus15 <qudusshonibare15@gmail.com>
2024-12-11 14:08:37 -0500

    Quiz App Updated

---
+++ quiz/FinalView.swift
@@ -0,0 +1,18 @@
+//
+//  FinalView.swift
+//  quiz
+//
+//  Created by Red Whale inc. on 26/11/2024.
+//
+
+import SwiftUI
+
+struct FinalView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FinalView()
+}
---
+++ quiz/QuizView.swift
@@ -0,0 +1,18 @@
+//
+//  QuizView.swift
+//  quiz
+//
+//  Created by Red Whale inc. on 26/11/2024.
+//
+
+import SwiftUI
+
+struct Quiz1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Quiz1()
+}
